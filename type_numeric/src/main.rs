1a665ea35292059351eb49968fa557fbddbf221a
yuzukicat <yuzuki.cat@kamisu66.com>
2023-08-09 18:07:13 +0900

    floating types

---
+++ type_numeric/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    integer_overflow();
+    compare_floating_point_type();
+}
+
+fn integer_overflow() {
+    let a: u8 = 255;
+    let b = a.wrapping_add(20);
+    println!("b: {}", b);
+}
+
+fn compare_floating_point_type() {
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("  0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("        0.3: {:x}", (abc.2).to_bits());
+    println!();
+    println!("xyz (f64)");
+    println!("  0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("        0.3: {:x}", (xyz.2).to_bits());
+
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    assert_ne!((xyz.0 + xyz.1), xyz.2);
+}
