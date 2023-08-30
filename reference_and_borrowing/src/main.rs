f6dbaf31d72c8790bb04f72f6b5978412799e9d5
yuzukicat <yuzuki.cat@kamisu66.com>
2023-08-30 17:46:24 +0900

    reference&borrowing-1

--- reference_and_borrowing/src/main.rs
+++ reference_and_borrowing/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    reference_and_dereference();
+    immutable_reference();
+    variable_reference();
+}
+
+fn reference_and_dereference() {
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn immutable_reference() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    print!("The length of {} is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn variable_reference() {
+    let mut s = String::from("hello");
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
--- reference_and_dereferencing/target/debug/deps/reference_and_dereferencing-c13aa7b94d57fc8a.d
+++
@@ -1,5 +0,0 @@
-/home/yuzuki/storage/personal/rust-yellow/reference_and_dereferencing/target/debug/deps/reference_and_dereferencing-c13aa7b94d57fc8a.rmeta: src/main.rs
-
-/home/yuzuki/storage/personal/rust-yellow/reference_and_dereferencing/target/debug/deps/reference_and_dereferencing-c13aa7b94d57fc8a.d: src/main.rs
-
-src/main.rs:
