4d9a6461dfdab92d90a429f26145be35a84d1a82
yuzukicat <yuzuki.cat@kamisu66.com>
2023-09-01 17:45:59 +0900

    ref complete && string

--- reference_and_borrowing/src/main.rs
+++ reference_and_borrowing/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     reference_and_dereference();
     immutable_reference();
     variable_reference();
+    memory_address();
+    ref_keyword();
+    borrowing_immutable_from_mutable_objects();
 }
 
 fn reference_and_dereference() {
@@ -29,3 +32,30 @@ fn variable_reference() {
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
+
+fn memory_address() {
+    let x = 5;
+    let p = &x;
+    println!("the memory address of x is {:p}", p);
+}
+
+fn ref_keyword() {
+    let c = 'z';
+    let r1 = &c;
+    let ref r2 = c;
+    assert_eq!(*r1, *r2);
+    assert_eq!(get_addr(r1), get_addr(r2));
+}
+
+// get memory address string
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
+
+fn borrowing_immutable_from_mutable_objects() {
+    let mut s = String::from("hello, ");
+    borrow_object(&s);
+    s.push_str("world");
+}
+
+fn borrow_object(s: &String) {}
